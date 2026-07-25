976f3b386f5dbbbe20ff762b35abee72c9a63b2a
Mohamed Elnaggar <>
2026-07-26 01:59:41 +0300

    practical exercise 01

--- Practical-Exercise-01/src/Main.java
+++ Practical-Exercise-01/src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 class Main {
 
     public static void main(String [] args) {
-        
-    }
+        // Display a message to command prompt
+        System.out.println("Practical Exercise 01");
 
+        Scanner scanner = new Scanner(System.in);
+
+        // Receive an input
+        System.out.print("Enter your name: ");
+        String clientName = scanner.nextLine();
+        System.out.println("Welcome to our exercise, " + clientName + "!");
+
+        // Receive 2 inputs number and string and print string
+        // according to given number
+        System.out.print("Enter your String: ");
+        String word = scanner.nextLine();
+
+        System.out.print("Enter a number to repeat your String: ");
+        int numberOfTimes = scanner.nextInt();
+
+        for(int i = 0; i < numberOfTimes; i++) {
+            System.out.println(word);
+        }
+
+        scanner.close();
+    }
 }
